6a92ab69ec2a8293fefc0c5c984e38a2096cfd0a
varunsrinivasan2 <varunsrinivasan2@gmail.com>
2022-03-15 16:20:13 -0700

    Complete bare minimum requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,18 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var pluckFirstLineFromFileAsync = require('./promiseConstructor.js').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification.js').getGitHubProfileAsync;
+var writeFileAsyc = Promise.promisify(fs.writeFile);
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then(getGitHubProfileAsync)
+    .then(function(profile) {
+      return writeFileAsyc(writeFilePath, JSON.stringify(profile));
+    });
 };
 
 // Export these functions so we can test them
